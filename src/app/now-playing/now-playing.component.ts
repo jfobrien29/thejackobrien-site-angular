2185db3877614dcf2fa1c07a0bb3d8e55fe46d51
Jack OBrien <thejackobrien29@gmail.com>
2018-10-24 00:41:37 -0400

    Added loader to Now Playing screen

--- src/app/now-playing/now-playing.component.ts
+++ src/app/now-playing/now-playing.component.ts
@@ -7,6 +7,8 @@ import { SpotifyService } from '../spotify.service';
   styleUrls: ['./now-playing.component.css']
 })
 export class NowPlayingComponent implements OnInit {
+  isLoading: boolean;
+  isPlaying: boolean;
   data: any;
   songTitle: string;
   songAlbumImage: string;
@@ -19,17 +21,24 @@ export class NowPlayingComponent implements OnInit {
   constructor(private spotifyService: SpotifyService) { }
 
   ngOnInit() {
+    this.isLoading = true;
     this.spotifyService.getSessionData().then(data => {
-      this.data = data;
-      this.songTitle = this.data.item.name;
-      this.songAlbumImage = this.data.item.album.images[0].url;
-      this.externalLink = this.data.item.external_urls.spotify;
-      this.spotifyUri = this.data.item.uri;
-      this.artists = this.data.item.artists.reduce((artistString, currArtist, index) => {
-        return (index === 0) ? currArtist.name : artistString + ', ' + currArtist.name;
-      }, '');
-      this.numArtists = this.data.item.artists.length;
-      this.review = this.data.review;
+      if (data !== 'Not Playing') {
+        this.isPlaying = true;
+        this.data = data;
+        this.songTitle = this.data.item.name;
+        this.songAlbumImage = this.data.item.album.images[0].url;
+        this.externalLink = this.data.item.external_urls.spotify;
+        this.spotifyUri = this.data.item.uri;
+        this.artists = this.data.item.artists.reduce((artistString, currArtist, index) => {
+          return (index === 0) ? currArtist.name : artistString + ', ' + currArtist.name;
+        }, '');
+        this.numArtists = this.data.item.artists.length;
+        this.review = this.data.review;
+      } else {
+        this.isPlaying = false;
+      }
+      setTimeout(() => {this.isLoading = false; }, 1000);
     });
   }
 }
