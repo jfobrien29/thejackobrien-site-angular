c87ab12a63a9e3cac248f1e6bf8440f92baa6bf9
Jack OBrien <thejackobrien29@gmail.com>
2018-10-24 16:14:53 -0400

    Edits to the footer

--- src/app/now-playing/now-playing.component.ts
+++ src/app/now-playing/now-playing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-now-playing',
@@ -13,12 +14,12 @@ export class NowPlayingComponent implements OnInit {
   songTitle: string;
   songAlbumImage: string;
   externalLink: string;
-  spotifyUri: string;
+  spotifyUri: SafeUrl;
   artists: string;
   hasMultipleArtists: boolean;
   review: string;
 
-  constructor(private spotifyService: SpotifyService) { }
+  constructor(private spotifyService: SpotifyService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.isLoading = true;
@@ -29,14 +30,19 @@ export class NowPlayingComponent implements OnInit {
         this.songTitle = this.data.item.name;
         this.songAlbumImage = this.data.item.album.images[0].url;
         this.externalLink = this.data.item.external_urls.spotify;
-        this.spotifyUri = this.data.item.uri;
+        this.spotifyUri = this.sanitize(this.data.item.uri);
         this.artists = this.data.item.artists.reduce((artistString, currArtist, index) => {
           return (index === 0) ? currArtist.name : artistString + ', ' + currArtist.name;
         }, '');
         this.hasMultipleArtists = this.data.item.artists.length > 1;
         this.review = this.data.review;
       }
-      setTimeout(() => {this.isLoading = false; }, 1500);
+
+      setTimeout(() => { this.isLoading = false; }, 1000);
     });
+  }
+
+  sanitize(uri: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(uri);
   }
 }
