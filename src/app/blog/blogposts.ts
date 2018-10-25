d89b064524b5e3a5ba8d733dc6191008b028d799
jfobrien29 <jfobrien@princeton.edu>
2018-10-24 21:46:09 -0400

    Updates to blog content

--- src/app/blog/blogposts.ts
+++ src/app/blog/blogposts.ts
@@ -9,56 +9,61 @@ const posts = [
     {
         imgLocation: 'https://i.imgur.com/NNy5eBJ.gif',
         date: 'date',
-        title: 'Now Playing Page',
+        title: 'Now Playing Page is Online',
         content: `In ancicipation of the 2018 New York Marathon, I wanted to create a fun app/tool/project to
-         share my experience with everyone else, especially those who generously gave! I came up with the idea to share
-         my music, and created a mini app using Spotify's developer APIs. Check it out now on the now playing tab! The app
-         wil display the song I am currently listening to, provide links if you would like to listen along.
-         For a few songs I've also written what goes through my head when it comes on, namely all songs on my
+         share my experience with everyone, especially those who generously gave to LLS! I came up with the idea to share
+         my music, and created a mini app using Spotify's developer APIs. Check it out now on the 
+         <a href="nowPlaying">now playing</a> tab! When I'm using spotify, the app
+         displays the song I am currently listening to and provides links to listen along.
+         For a small subset of songs you'll also see my thoughts, namely all songs on my
          <a href=https://open.spotify.com/user/12137830555/playlist/2JUf2gUEcVwxOue6vaASwa?si=fJzXY5RQRua6JmYtVmXZfQ>
-        "GAL 26.2"</a> marathon playlist.`
+        "GAL 26.2"</a> marathon playlist. Listen along!`
     },
     {
         imgLocation: 'https://i.imgur.com/AeA81hc.png',
         date: 'date',
         title: 'Groovin Against Leukemia',
         content: `In November 2017, I watched the New York Marathon and loved it. The scale of the race is incredible,
-         it's inspiring, and I've always toyed around with doing one just to prove I could. 6 months, 300+ miles, and $35,000+ later
-         and I am well on my way!
-
-         I started <em>Groovin Against Leukemia</em> with a few friends and we've crushed our training and raised more than we thought
-         possible for the Leukemai Lymphoma Society. The whole process has been incredible, and I've been
+         the runners are inspiring, and I've always toyed around with doing one just to prove I could. 6 months, 300+ miles, 
+         and $35,000+ later and I'm well on my way!
+         I started <em>Groovin Against Leukemia</em> back in March with a few friends, and since then 
+         we've crushed our training and raised more than we thought possible for the Leukemai Lymphoma Society. 
+         The whole process has been incredible, and I've been
          <a href="">documenting it all</a> if you want to read more or donate!!`
     },
     {
         imgLocation: 'https://i.imgur.com/lq6qpxT.jpg',
         date: 'date',
         title: 'IBM Blockchain: State Of Delaware',
-        content: `Since starting with IBM in the fall of 2017, the first project I can comfortably write about on the internet is
+        content: `Since starting with IBM Blockchain in the fall of 2017, the first project I can talk about publicly is
          my project for the State of Delaware. Thanks to the freedom of information act, you can read about the project
-         <a href="https://tinyurl.com/y7ogmqxs">here</a>. The project stems from an issue Dole ran into when tracking down its
-         shareholders in a class action law suit a few years ago (<a href="https://tinyurl.com/yamfptd9">solid article</a>).
-         We are aiming to build a blockchain system for Delaware, where upwards of %50 of US companies are incorporated,
-         to track the authorization and issuance of stock between multiple corporations, shareholders, law firms, and
-         registered agents. Delware is looking to become the first "Blockchain" state!`
+         <a href="https://tinyurl.com/y7ogmqxs">here</a>. The project stems from an issue Dole (yes, the fruit company)
+         ran into when tracking down its
+         shareholders in a class action law suit a few years back (<a href="https://tinyurl.com/yamfptd9">NY Times Read</a>).
+         We're trying to make sure problems like this don't happen again, and make life easier for everyone involved.
+         Over the past few months we've built a blockchain network for Delaware
+         to track the authorization and issuance of stock between corporations, shareholders, law firms, and
+         registered agents. Delware is looking to become the first blockchain-enabled state!`
     },
     {
         imgLocation: 'https://i.imgur.com/qeGX1tj.jpg',
         date: 'date',
         title: 'Old Relly: School Bus Rennovation Project',
-        content: `I could write a whole blog on the Reller, and someday I will. I have somewhat detailed notes from every day of our
+        content: `I could write a whole blog on the Reller, and someday I will. I have detailed notes from every day of our
          cross country road trip, loads of resources from our build, and hundreds of pictures (thousands if you include
             <a href="http://www.beargoldstein.com/">Bear Goldstien's Photos</a>).
-         For now just check out some pictures and videos of the legendary
+         For now, you can check out select pictures and videos of our legendary
          bus on our <a href="https://www.instagram.com/oldrelly/">instagram page</a>.`
     },
     {
         imgLocation: 'https://i.imgur.com/dXoMr47.png',
         date: 'date',
         title: 'Tiger Bed Rentals',
-        content: `Adam Hardej and I started our first business, Tiger Bed Rentals LLC, in the Summer of 2016. We offered full XL mattresses
-         to college students at low prices and handled all move-in, move-out, and storage. "You rent. You sleep. We do the rest." After
-         we graduated from college, Adam took reigns of the business, which he eventually sold to Roommie Rents
+        content: `Adam Hardej and I started our first business, Tiger Bed Rentals LLC, in the Summer of 2016. Our business
+         offered full XL mattresses
+         to college students at low prices and handled move-in, move-out, and summer storage. We thrived under the motto
+         "You rent. You sleep. We do the rest." After
+         we graduated from college, Adam took rthe eigns of the business and eventually sold it Roommie Rentals
          in the fall of 2018. Check out the <a href="tigerbedrentals.com">website for TBR</a>
          (which I assume is still operational) and a write up on Tiger Beds from
          <a href="https://kellercenter.princeton.edu/elab/tiger-bed-rentals-llc">Princeton's entreprenourship hub</a>.`
@@ -67,7 +72,7 @@ const posts = [
         imgLocation: '../../assets/me.png',
         date: 'date',
         title: 'Other...',
-        content: `I'll think of some other stuff to throw in here... The page is still very much WIP!`
+        content: `I'll think of some other great bits to throw in here... see you back soon!`
     }
 ];
 
