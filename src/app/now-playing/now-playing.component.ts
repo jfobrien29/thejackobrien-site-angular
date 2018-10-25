b9aeb3a8fbe6a84822c9da71def39b1dc8bf7a6a
jfobrien29 <jfobrien@princeton.edu>
2018-10-24 23:25:35 -0400

    Added automatic refresh to now playing

--- src/app/now-playing/now-playing.component.ts
+++ src/app/now-playing/now-playing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Song } from './song';
+import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-now-playing',
@@ -13,20 +14,38 @@ export class NowPlayingComponent implements OnInit {
   isPlaying: boolean;
   data: any;
   song: Song;
+  interval: any;
 
   constructor(private spotifyService: SpotifyService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.isLoading = true;
+
     this.spotifyService.getSessionData().then(data => {
       this.data = data;
       this.isPlaying = this.data.isPlaying;
-      if (this.isPlaying) {
 
+      if (this.isPlaying) {
         this.song = new Song(this.data, this.sanitizer);
       }
 
       setTimeout(() => { this.isLoading = false; }, 1000);
+
+      this.interval = setInterval(() => {
+        console.log('Refresh Data');
+        this.refreshData();
+      }, 30000);
+    });
+
+  }
+
+  refreshData() {
+    this.spotifyService.getSessionData().then(data => {
+      this.data = data;
+      this.isPlaying = this.data.isPlaying;
+      if (this.isPlaying) {
+        this.song = new Song(this.data, this.sanitizer);
+      }
     });
   }
 }
