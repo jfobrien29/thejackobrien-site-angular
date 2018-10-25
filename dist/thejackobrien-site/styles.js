83807cb4b4eee5df8c80e209245b67842f370f49
jfobrien29 <jfobrien@princeton.edu>
2018-10-25 00:16:38 -0400

    Added google analytics

--- dist/thejackobrien-site/styles.js
+++ dist/thejackobrien-site/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/John.OBrien@ibm.com/Dev/heroku/personal-site-angular/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/jackobrien/dev/thejackobrien-site-angular/src/styles.css */"./src/styles.css");
 
 
 /***/ })
