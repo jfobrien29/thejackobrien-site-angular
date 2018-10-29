4dd08e281df2bfa1db24ea96bbdc4d3721b3c0b5
John-OBrien <thejackobrien29@gmail.com>
2018-10-29 15:10:20 -0400

    Typo fixes

--- src/app/blog/blogposts.ts
+++ src/app/blog/blogposts.ts
@@ -3,17 +3,17 @@ const posts = [
         imgLocation: '../../assets/me.png',
         date: 'date',
         title: 'Welcome to my "Blog"!',
-        content: `This isn\'t really a blog, it\'s more of a collection of events
-         and projects that I felt were worthwhile to put on the internet. Check it out and let me know what you think!`
+        content: `Well this isn't really a blog, it's more of a collection of events
+         and projects that I felt were worthwhile to share on the internet. Check it out and let me know what you think!`
     },
     {
         imgLocation: 'https://i.imgur.com/NNy5eBJ.gif',
         date: 'date',
-        title: 'Now Playing Page is Online',
-        content: `In ancicipation of the 2018 New York Marathon, I wanted to create a fun app/tool/project to
+        title: 'Now Playing Page is Online!',
+        content: `In ancicipation of the 2018 New York Marathon, I wanted to create a fun app/project to
          share my experience with everyone, especially those who generously gave to LLS! I came up with the idea to share
-         my music, and created a mini app using Spotify's developer APIs. Check it out now on the 
-         <a href="nowPlaying">now playing</a> tab! When I'm using spotify, the app
+         my music, and created a mini app using Spotify's developer APIs. Check it out now on the
+         <a href="nowPlaying">Now Playing</a> tab! When I'm using spotify, the app
          displays the song I am currently listening to and provides links to listen along.
          For a small subset of songs you'll also see my thoughts, namely all songs on my
          <a href=https://open.spotify.com/user/12137830555/playlist/2JUf2gUEcVwxOue6vaASwa?si=fJzXY5RQRua6JmYtVmXZfQ>
@@ -23,11 +23,11 @@ const posts = [
         imgLocation: 'https://i.imgur.com/AeA81hc.png',
         date: 'date',
         title: 'Groovin Against Leukemia',
-        content: `In November 2017, I watched the New York Marathon and loved it. The scale of the race is incredible,
-         the runners are inspiring, and I've always toyed around with doing one just to prove I could. 6 months, 300+ miles, 
-         and $35,000+ later and I'm well on my way!
-         I started <em>Groovin Against Leukemia</em> back in March with a few friends, and since then 
-         we've crushed our training and raised more than we thought possible for the Leukemai Lymphoma Society. 
+        content: `In November 2017, I watched the New York Marathon and loved it. The scale of the race is incredible
+         and I've always toyed around with doing one to prove I could. 6 months, 300+ miles,
+         and $35,000+ in fundraising later and I'm well on my way!
+         I started <em>Groovin Against Leukemia</em> back in March 2018 with a few friends, and since then
+         we've crushed our training and raised more than we thought possible for the Leukemia Lymphoma Society.
          The whole process has been incredible, and I've been
          <a href="">documenting it all</a> if you want to read more or donate!!`
     },
@@ -40,7 +40,6 @@ const posts = [
          <a href="https://tinyurl.com/y7ogmqxs">here</a>. The project stems from an issue Dole (yes, the fruit company)
          ran into when tracking down its
          shareholders in a class action law suit a few years back (<a href="https://tinyurl.com/yamfptd9">NY Times Read</a>).
-         We're trying to make sure problems like this don't happen again, and make life easier for everyone involved.
          Over the past few months we've built a blockchain network for Delaware
          to track the authorization and issuance of stock between corporations, shareholders, law firms, and
          registered agents. Delware is looking to become the first blockchain-enabled state!`
@@ -60,10 +59,10 @@ const posts = [
         date: 'date',
         title: 'Tiger Bed Rentals',
         content: `Adam Hardej and I started our first business, Tiger Bed Rentals LLC, in the Summer of 2016. Our business
-         offered full XL mattresses
+         offered full XL mattresses rentals
          to college students at low prices and handled move-in, move-out, and summer storage. We thrived under the motto
          "You rent. You sleep. We do the rest." After
-         we graduated from college, Adam took rthe eigns of the business and eventually sold it Roommie Rentals
+         we graduated from college, Adam took the reigns of the business and eventually sold it Roommie Rentals
          in the fall of 2018. Check out the <a href="tigerbedrentals.com">website for TBR</a>
          (which I assume is still operational) and a write up on Tiger Beds from
          <a href="https://kellercenter.princeton.edu/elab/tiger-bed-rentals-llc">Princeton's entreprenourship hub</a>.`
