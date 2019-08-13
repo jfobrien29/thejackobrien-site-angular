8fda5896bb0f74bc359a4ee44d0fc32a3b80f13d
jfobrien29 <thejackobrien29@gmail.com>
2019-08-12 23:46:14 -0400

    Add long awaited updates to site

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent {
+  resumeAlert(): void {
+    alert('Resume not currently available. Email me for more info!');
   }
-
 }
